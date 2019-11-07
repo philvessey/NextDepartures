e30a1a50e2d4be3df22d72dfa55c0329c2829776
hypervtechnics <hypervtechnics@gmail.com>
2019-11-07 16:05:39 +0100

    Add weekday utils

---
+++ NextDepartures.Standard/Utils/WeekdayUtils.cs
@@ -0,0 +1,57 @@
+﻿using NextDepartures.Standard.Models;
+using System;
+using System.Collections.Generic;
+
+namespace NextDepartures.Standard.Utils
+{
+    public static class WeekdayUtils
+    {
+        private static readonly Dictionary<DayOfWeek, Func<Departure, string>> previousDays = new Dictionary<DayOfWeek, Func<Departure, string>>()
+        {
+            { DayOfWeek.Monday, d => d.Sunday },
+            { DayOfWeek.Tuesday, d => d.Monday },
+            { DayOfWeek.Wednesday, d => d.Tuesday },
+            { DayOfWeek.Thursday, d => d.Wednesday },
+            { DayOfWeek.Friday, d => d.Thursday },
+            { DayOfWeek.Saturday, d => d.Friday },
+            { DayOfWeek.Sunday, d => d.Saturday },
+        };
+
+        private static readonly Dictionary<DayOfWeek, Func<Departure, string>> todayDays = new Dictionary<DayOfWeek, Func<Departure, string>>()
+        {
+            { DayOfWeek.Monday, d => d.Monday },
+            { DayOfWeek.Tuesday, d => d.Tuesday },
+            { DayOfWeek.Wednesday, d => d.Wednesday },
+            { DayOfWeek.Thursday, d => d.Thursday },
+            { DayOfWeek.Friday, d => d.Friday },
+            { DayOfWeek.Saturday, d => d.Saturday },
+            { DayOfWeek.Sunday, d => d.Sunday },
+        };
+
+        private static readonly Dictionary<DayOfWeek, Func<Departure, string>> followingDays = new Dictionary<DayOfWeek, Func<Departure, string>>()
+        {
+            { DayOfWeek.Monday, d => d.Tuesday },
+            { DayOfWeek.Tuesday, d => d.Wednesday },
+            { DayOfWeek.Wednesday, d => d.Thursday },
+            { DayOfWeek.Thursday, d => d.Friday },
+            { DayOfWeek.Friday, d => d.Saturday },
+            { DayOfWeek.Saturday, d => d.Sunday },
+            { DayOfWeek.Sunday, d => d.Monday },
+        };
+
+        public static string GetPreviousDay(DayOfWeek dayOfWeek, Departure departure)
+        {
+            return previousDays[dayOfWeek](departure);
+        }
+
+        public static string GetTodayDay(DayOfWeek dayOfWeek, Departure departure)
+        {
+            return todayDays[dayOfWeek](departure);
+        }
+
+        public static string GetFollowingDay(DayOfWeek dayOfWeek, Departure departure)
+        {
+            return followingDays[dayOfWeek](departure);
+        }
+    }
+}
