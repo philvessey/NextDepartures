5893c9874317ce3e8879dcc2e512e68515a9fb75
Phil Vessey <philvessey@icloud.com>
2020-04-12 19:53:46 +0100

    Update service endpoints with new overloading.

--- NextDepartures.Standard/GetServicesByParentStationAsync.cs
+++ NextDepartures.Standard/GetServicesByParentStationAsync.cs
@@ -73,5 +73,68 @@ namespace NextDepartures.Standard
                 return null;
             }
         }
+
+        /// <summary>
+        /// Gets the services for a parent station.
+        /// </summary>
+        /// <param name="stopsByParentStation">A list of stops to group as a parent station.</param>
+        /// <param name="count">The number of results to return. Default is all (0) but can be overridden.</param>
+        /// <returns>A list of services.</returns>
+        public Task<List<Service>> GetServicesByParentStationAsync(List<Stop> stopsByParentStation, int count = 0)
+        {
+            return GetServicesByParentStationAsync(stopsByParentStation, DateTime.Now, count);
+        }
+
+        /// <summary>
+        /// Gets the services for a parent station.
+        /// </summary>
+        /// <param name="stopsByParentStation">A list of stops to group as a parent station.</param>
+        /// <param name="now">The DateTime target to search from.</param>
+        /// <param name="count">The number of results to return. Default is all (0) but can be overridden.</param>
+        /// <returns>A list of services.</returns>
+        public async Task<List<Service>> GetServicesByParentStationAsync(List<Stop> stopsByParentStation, DateTime now, int count = 0)
+        {
+            const int ToleranceInHours = 12;
+
+            try
+            {
+                List<Agency> agencies = await _dataStorage.GetAgenciesAsync();
+                List<CalendarDate> calendarDates = await _dataStorage.GetCalendarDatesAsync();
+                List<Stop> stops = await _dataStorage.GetStopsAsync();
+
+                List<Departure> departuresForStation = new List<Departure>();
+
+                foreach (Stop stop in stopsByParentStation)
+                {
+                    List<Departure> departuresFromStorage = await _dataStorage.GetDeparturesForStopAsync(stop.Id);
+
+                    departuresForStation.AddRange(new List<Departure>()
+                        .AddMultiple(GetDeparturesOnDay(agencies, calendarDates, stops, departuresFromStorage, now, DayOffsetType.Yesterday, ToleranceInHours, stop.Id))
+                        .AddMultiple(GetDeparturesOnDay(agencies, calendarDates, stops, departuresFromStorage, now, DayOffsetType.Today, ToleranceInHours, stop.Id))
+                        .AddMultiple(GetDeparturesOnDay(agencies, calendarDates, stops, departuresFromStorage, now, DayOffsetType.Tomorrow, ToleranceInHours, stop.Id))
+                        );
+                }
+
+                if (count > 0)
+                {
+                    return departuresForStation
+                        .OrderBy(d => d.DepartureDateTime)
+                        .Take(count)
+                        .Select(d => CreateService(agencies, stops, d))
+                        .ToList();
+                }
+                else
+                {
+                    return departuresForStation
+                        .OrderBy(d => d.DepartureDateTime)
+                        .Select(d => CreateService(agencies, stops, d))
+                        .ToList();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
--- NextDepartures.Standard/GetServicesByStopAsync.cs
+++ NextDepartures.Standard/GetServicesByStopAsync.cs
@@ -66,5 +66,62 @@ namespace NextDepartures.Standard
                 return null;
             }
         }
+
+        /// <summary>
+        /// Gets the services for a specific stop.
+        /// </summary>
+        /// <param name="stop">The stop.</param>
+        /// <param name="count">The number of results to return. Default is all (0) but can be overridden.</param>
+        /// <returns>A list of services.</returns>
+        public Task<List<Service>> GetServicesByStopAsync(Stop stop, int count = 0)
+        {
+            return GetServicesByStopAsync(stop, DateTime.Now, count);
+        }
+
+        /// <summary>
+        /// Gets the services for a specific stop.
+        /// </summary>
+        /// <param name="stop">The stop.</param>
+        /// <param name="now">The DateTime target to search from.</param>
+        /// <param name="count">The number of results to return. Default is all (0) but can be overridden.</param>
+        /// <returns>A list of services.</returns>
+        public async Task<List<Service>> GetServicesByStopAsync(Stop stop, DateTime now, int count = 0)
+        {
+            const int ToleranceInHours = 12;
+
+            try
+            {
+                List<Agency> agencies = await _dataStorage.GetAgenciesAsync();
+                List<CalendarDate> calendarDates = await _dataStorage.GetCalendarDatesAsync();
+                List<Departure> departuresFromStorage = await _dataStorage.GetDeparturesForStopAsync(stop.Id);
+                List<Stop> stops = await _dataStorage.GetStopsAsync();
+
+                List<Departure> departuresForStop = new List<Departure>();
+
+                departuresForStop.AddRange(new List<Departure>()
+                    .AddMultiple(GetDeparturesOnDay(agencies, calendarDates, stops, departuresFromStorage, now, DayOffsetType.Yesterday, ToleranceInHours, stop.Id))
+                    .AddMultiple(GetDeparturesOnDay(agencies, calendarDates, stops, departuresFromStorage, now, DayOffsetType.Today, ToleranceInHours, stop.Id))
+                    .AddMultiple(GetDeparturesOnDay(agencies, calendarDates, stops, departuresFromStorage, now, DayOffsetType.Tomorrow, ToleranceInHours, stop.Id))
+                    );
+
+                if (count > 0)
+                {
+                    return departuresForStop
+                        .Take(count)
+                        .Select(d => CreateService(agencies, stops, d))
+                        .ToList();
+                }
+                else
+                {
+                    return departuresForStop
+                        .Select(d => CreateService(agencies, stops, d))
+                        .ToList();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
